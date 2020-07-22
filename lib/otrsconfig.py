104ea0cbfde41f244d11687c6585076d30890c06
pritishpattanaik <pattanaikpritish@gmail.com>
2020-07-22 16:27:30 +0800

    Create otrsconfig.py

---
+++ lib/otrsconfig.py
@@ -0,0 +1,8 @@
+config = {
+    'user'  : 'otrsuser', 
+    'pw':   'otrspw',
+    'url': 'otrs url',
+    'sessionroute': '/Session/GetToken?',  # get session, this should match your actual endpoint with route 
+    'ticketgetroute': '/Ticket/', # get ticket details, 
+    'createticketroute': '/Ticket/TicketCreate', # Ticket Create 
+}
